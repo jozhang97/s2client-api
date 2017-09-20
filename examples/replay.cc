962aa82ce7a6281d5e7139f2db5a4a44928c3ff8
Jeffrey Zhang <jozhang@berkeley.EDU>
2017-09-19 21:21:59 -0400

    Start multiagent game after collect replays. Problem with network?

--- examples/replay.cc
+++ examples/replay.cc
@@ -3,6 +3,7 @@
 #include "sc2utils/sc2_manage_process.h"
 
 #include <iostream>
+#include <queue>
 
 using namespace sc2;
 
@@ -21,6 +22,7 @@ const char* kReplayFolder = "/home/jeffrey/MyReplays/";
 class Replay : public sc2::ReplayObserver {
 public:
     std::vector<uint32_t> count_units_built_;
+    std::queue<RawActions> raw_actions_array;
 
     Replay() :
         sc2::ReplayObserver() {
@@ -39,7 +41,18 @@ public:
     }
 
     void OnStep() final {
-        printf("STEP");
+        const sc2::ObservationInterface* obs = Observation();
+        const RawActions& raw = obs->GetRawActions();
+
+        for (int i = 0; i < raw.size(); i++) {
+            printf("Ability_id %d. ", raw[i].ability_id);
+            for (int j = 0; j < raw[i].unit_tags.size(); j++) {
+                printf("Tags %d ", raw[i].unit_tags[j]);
+            }
+        }
+        raw_actions_array.push(raw); // make sure don't need reserve()
+        printf("ONSTEP LOOK AT ACTIONS %d \n", raw.size());
+            
     }
 
     void OnGameEnd() final {
@@ -55,39 +68,69 @@ public:
         }
         std::cout << "Finished" << std::endl;
     }
+
+    RawActions GetNextRawActions() {
+        RawActions ret = raw_actions_array.front();
+        raw_actions_array.pop();
+        return ret;
+    }
 };
 
+class Bot : public Agent {
+public: 
+    virtual void OnGameStart() final {
+        printf("Multiplayer game started");
+    }
+    virtual void OnStep() final {
+        printf("Multiplayer game stepping");
+    }
+};
 
 int main(int argc, char* argv[]) {
-    sc2::Coordinator coordinator;
-    if (!coordinator.LoadSettings(argc, argv)) {
+    int stop_iter = 100;
+    int i = 0;
+    sc2::Coordinator replay_coordinator;
+    sc2::Coordinator multiagent_coordinator;
+    Replay replay_observer;
+    Bot bot; 
+
+    printf("Starting replay \n ");
+    if (!replay_coordinator.LoadSettings(argc, argv)) {
         return 1;
     }
-
-    if (!coordinator.SetReplayPath(kReplayFolder)) {
+    if (!replay_coordinator.SetReplayPath(kReplayFolder)) {
         printf("Replay Folder: %s \n", kReplayFolder);
         std::cout << "Unable to find replays." << std::endl;
         return 1;
     }
+    replay_coordinator.AddReplayObserver(&replay_observer);
 
-    Replay replay_observer;
-
-    coordinator.AddReplayObserver(&replay_observer);
-/*
-
-    coordinator.SetParticipants({
-        CreateComputer(Race::Zerg),
-        CreateComputer(Race::Terran)
-    });
-
-    coordinator.LaunchStarcraft();
-    coordinator.StartGame(sc2::kMapBelShirVestigeLE);
-*/
-
-    int i = 0;
-    while (i++ < 100){
-        coordinator.Update();
+    while (i++ < stop_iter){
+        replay_coordinator.Update();
     }
-    printf("DONE in %d ticks\n", i);
+    replay_coordinator.LeaveGame();
+    printf("Finished replay with %d ticks \n ", i);
+
+    
+    replay_coordinator.WaitForAllResponses();
+    multiagent_coordinator.WaitForAllResponses();
     while (!sc2::PollKeyPress());
+
+
+    printf("Starting multiplayer game \n ");
+    if (!multiagent_coordinator.LoadSettings(argc, argv)) {
+        return 1;
+    }
+    multiagent_coordinator.SetParticipants({
+        CreateComputer(Race::Zerg),
+        CreateParticipant(Race::Terran, &bot)
+    });
+    multiagent_coordinator.LaunchStarcraft();
+
+    multiagent_coordinator.StartGame(sc2::kMapBelShirVestigeLE);
+    i = 0;
+    while (i++ < stop_iter){
+        // pass in actions here 
+        replay_coordinator.Update();
+    }
 }
